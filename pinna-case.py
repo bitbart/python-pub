bb8cec6c6e77f3da55d4c654cfa87361cedc12ca
Massimo Bartoletti <bart@unica.it>
2022-10-25 20:02:17 +0200

    pinna-case

--- pinna-case.py
+++ pinna-case.py
@@ -1,30 +1,45 @@
+"""
+An atrocious crime was committed last night at the Science Palace: 
+Prof. Pinna was found dead in a pool of blood. The police,
+after a throughout inspection, has come to the following conclusions:
+1. If the murder was committed after midnight, then the crime scene
+cannot be the Batcave, or Prof. Scateni is innocent.
+2. If Prof. Scateni is guilty, then the murder weapon is a 3D printer.
+3. If the murder weapon is a 3D printer and the murder was committed
+in the Batcave, then the time of the murder is after midnight.
+Assume that the police eventually discovers that the murder was committed in the Batcave. 
+After knowing this fact, can we infer that Prof. Scateni is innocent?
+"""
+
 from z3 import *
 
-# m = the murder was committed after midnight;
-m = Bool('m')
+# M = the murder was committed after midnight;
+M = Bool('M')
 
-# b = the crime scene is the Batcave;
-b = Bool('b')
+# B = the crime scene is the Batcave;
+B = Bool('B')
 
-# i = Prof.\ Scateni is innocent;
-i = Bool('i')
+# P = Prof. Scateni is innocent;
+I = Bool('I')
 
-# p = the murder weapon is a 3D printer.
-p = Bool('p')
-
-# m => not b or i
-# not i => p
-# (p and b) => m
-# b
+# P = the murder weapon is a 3D printer.
+P = Bool('P')
 
 s = Solver()
 
-s.add(If(m,Not(b),i))
-s.add(Implies(Not(i),p))
-s.add(Implies(And(p,b),m))
-s.add(b)
+# M => not B or I
+s.add(Implies(M,Or(Not(B),I)))
 
-if s.check(Not(i))==unsat:
+# not I => P
+s.add(Implies(Not(I),P))
+
+# (P and B) => M
+s.add(Implies(And(P,B),M))
+
+# B
+s.add(B)
+
+if s.check(Not(I))==unsat:
     print("Prof. Scateni is innocent")
 else:
     print("Prof. Scateni could be guilty")
